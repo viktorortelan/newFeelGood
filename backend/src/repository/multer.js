deb0a4696fb202d131d54370f23b99687d3848b5
viktorortelan <jvortelan@gmail.com>
2024-10-12 20:50:26 -0300

    .

--- backend/src/repository/multer.js
+++ backend/src/repository/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../stratton_oakmonk/public/imgs');
+    cb(null, '../frontend/public/imgs');
   },
   filename: function (req, file, cb) {
     const type = file.originalname.split('.').pop();
